b4ea22cf38656684d483979f33ebfcd018055d8e
isha056 <115340328+isha056@users.noreply.github.com>
2022-12-01 22:42:07 +0530

    Create Lecture-7.py

---
+++ Lecture-7.py
@@ -0,0 +1,101 @@
+#LOOPING STATEMENTS
+
+
+#WHILE LOOP STATEMENT
+a=1
+while a<10:
+  print(a)
+  a += 1
+
+#while battery.notDead():
+  pass
+
+# FOR LOOP STATEMENT
+iter("Isha")
+
+#a=1
+#print(a.__iter__)
+
+a= "Isha"
+print(a.__iter__)
+for c in a:
+  print(c)
+  print(type(c))
+print(type(a))
+print(type(c))
+
+for i in  range (10):
+  print(i)
+
+#for i in  1:
+ # print(i)
+
+a=range(2)
+print(a)
+
+#BREAK , CONTINUE , PASS
+print("aaaa"*4)
+
+for i in range (2):
+  if i==1:
+    continue
+  print(i)
+
+for i in range(2):
+  print(i)
+  if i ==1:
+    continue
+
+for i in range(2):
+  print(i)
+  if i ==1:
+    break
+
+for i in range(2):# i = 0 ,#i=1,.....
+  print(i)#i=0 ,i=1,.....
+  i = 100 #i=100 , i=100,.....
+  print(i) #i=100 , i=100,.....
+
+for i in range(2):
+  print(i)
+  del i
+
+"""
+#PASS
+#Python want atleast one code after if statement we can left that empty
+ #to do empty code we use pass statement
+"""
+
+#PASS
+#Python want atleast one code after if statement we can left that empty
+ #to do empty code we use pass statement
+#if True:
+  # I DON'T KNOW WHAT TO DO
+#print("rest of the code")
+
+if True:
+  "Isha Sharma"
+print("rest of the code")
+
+if True:
+  # I DON'T KNOW WHAT TO DO
+  pass
+print("rest of the code")
+
+for i in range(2):
+  print(i)
+else:
+  print("Isha")
+
+for i in range(2):
+  print(i)
+  if i ==1:
+    break
+else:
+  print("Isha")
+
+for i in range(2):
+  print(i)
+  if i ==1:
+    break
+print("Isha")
